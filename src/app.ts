0a46f3cdc0c4dc298f297e4b6c052d904057bf1a
brunovinicius <brunoviniciustica@gmail.com>
2022-10-08 12:50:23 +0100

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import product from './routes/Product.routes';
 import * as bodyParser from "body-parser";
+import * as cors from 'cors';
 import * as dotenv from "dotenv";
 
 export class App {
@@ -19,7 +20,22 @@ export class App {
   }
 
   private middlewares(): void {
+
+    // const allowedOrigins = ['http://localhost:5173'];
+    const allowedContentTypes = ['Content-Type'];
+    const options: cors.CorsOptions = {
+      // origin: allowedOrigins,
+      methods: "GET, OPTIONS, PUT, POST, DELETE",
+      allowedHeaders: allowedContentTypes,
+      credentials : true
+    };
+
     this.express.use(bodyParser.json());
+    this.express.use(cors(options));
+
+    // this.express.use(cors({
+    //    origin: "http://localhost:5173"
+    // }))
     dotenv.config();
     this.express.use(express.urlencoded({ extended: true }));
     this.express.use(express.json());
